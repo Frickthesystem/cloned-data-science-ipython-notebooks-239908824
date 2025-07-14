495875b904222d55466138cae38400dd03a9b2be
Frickthesystem <frickthesystemio@gmail.com>
2025-07-14 17:21:25 +0000

    🐍 Add Python source files (batch 9/48) from donnemartin/data-science-ipython-notebooks

---
+++ python-data/type_util.py
@@ -0,0 +1,28 @@
+# Customized from donnemartin/data-science-ipython-notebooks
+# Original: https://github.com/donnemartin/data-science-ipython-notebooks
+# Cloned on: 2025-07-14
+
+class TypeUtil:
+
+    @classmethod
+    def is_iterable(cls, obj):
+        """Determines if obj is iterable.
+
+        Useful when writing functions that can accept multiple types of
+        input (list, tuple, ndarray, iterator).  Pairs well with
+        convert_to_list.
+        """
+        try:
+            iter(obj)
+            return True
+        except TypeError:
+            return False
+
+    @classmethod
+    def convert_to_list(cls, obj):
+        """Converts obj to a list if it is not a list and it is iterable, 
+        else returns the original obj.
+        """
+        if not isinstance(obj, list) and cls.is_iterable(obj):
+            obj = list(obj)
+        return obj
