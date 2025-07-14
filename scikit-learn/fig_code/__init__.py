7c33fa150091856f4cfc55db456416b21234881e
Frickthesystem <frickthesystemio@gmail.com>
2025-07-14 17:21:38 +0000

    🐍 Add Python source files (batch 10/48) from donnemartin/data-science-ipython-notebooks

---
+++ scikit-learn/fig_code/__init__.py
@@ -0,0 +1,10 @@
+# Customized from donnemartin/data-science-ipython-notebooks
+# Original: https://github.com/donnemartin/data-science-ipython-notebooks
+# Cloned on: 2025-07-14
+
+from .data import *
+from .figures import *
+
+from .sgd_separator import plot_sgd_separator
+from .linear_regression import plot_linear_regression
+from .helpers import plot_iris_knn
